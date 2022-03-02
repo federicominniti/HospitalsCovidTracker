b689ee581d4d2d2f2a40dfaaceb2055e57384998
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-03-02 18:04:34 +0100

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,15 +1,10 @@
 # 1. COMPILAZIONE
-# Il comando 'make' necessita del makefile, che deve essere
-# creato come descritto nella guida sulla pagina Elearn
 
   make
 
   read -p "Compilazione eseguita. Premi invio per eseguire..."
 
 # 2. ESECUZIONE
-# I file eseguibili di discovery server e peer devono
-# chiamarsi 'ds' e 'peer', e devono essere nella current folder
-
 # 2.1 esecuzioe del DS sulla porta 4242
   gnome-terminal -x sh -c "./ds 4242; exec bash"
 
